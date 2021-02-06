fe015cb6347bb2fb1e4ccaae52337e9c7fcf8ca0
Akbarovv <asa33.15@mail.ru>
2021-02-06 23:02:29 +0500

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import {HashRouter, Route} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
@@ -17,7 +17,7 @@ type AppPropsType = {
 const App: React.FC<AppPropsType> = (props) => {
     const state = props.store.getState()
     return (
-        <BrowserRouter>
+        <HashRouter>
             <div className='app-wrapper'>
                 <Header/>
                 <Navbar/>
@@ -31,7 +31,7 @@ const App: React.FC<AppPropsType> = (props) => {
                     <Route path='/settings' component={Settings}/>
                 </div>
             </div>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 export default App;
